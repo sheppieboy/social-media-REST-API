13490a4ce0a42ffa9f54c5309ce33aab1ea368e4
Luke Sheppard <sheppiegit@gmail.com>
2023-01-24 22:50:13 -0500

    Added a models folder for the mongoDB schema models.  Added the User mongoDB schema.

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const { required } = require("nodemon/lib/config");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 50,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 50,
+      unique: true,
+    },
+
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
