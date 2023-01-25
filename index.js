f8c22e96c41676bcefb6f4d7d679c4ebc78d705c
Luke Sheppard <sheppiegit@gmail.com>
2023-01-25 12:16:09 -0500

    Updated auth route and user route, and added error logging from mongoDb

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const helmet = require("helmet");
+//routes
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 const server = express();
 
-mongoose.connect(process.env.MONGO_URL, () => {
+mongoose.connect(process.env.MONGO_URL, (err) => {
+  if (err) console.log(err);
   console.log("Connected to mongoDB");
 });
 
@@ -19,6 +23,9 @@ const port = 3000;
 server.use(express.json());
 server.use(helmet());
 server.use(morgan("common"));
+
+server.use("/api/users", userRoute);
+server.use("/api/auth", authRoute);
 
 server.listen(port, () => {
   console.log(`backend server is ready to rumble @ port ${port}!`);
