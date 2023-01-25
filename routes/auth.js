3b59a93b0da54d166f4f6885763604881e4fb930
Luke Sheppard <sheppiegit@gmail.com>
2023-01-25 12:21:49 -0500

    Updated auth route to add a new User to the database as a test.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("auth route");
+const User = require("../models/User");
+
+router.get("/", async (req, res) => {
+  const user = await new User({
+    username: "Luke",
+    email: "lukesheppard@gmail.com",
+    password: "password123",
+  });
+
+  await user.save();
+  res.send("test user");
 });
 
 module.exports = router;
