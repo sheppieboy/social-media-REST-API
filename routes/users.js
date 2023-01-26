33adeb25517fedea0c1813a8295ca9aa93fbea64
Luke Sheppard <sheppiegit@gmail.com>
2023-01-25 19:04:19 -0500

    Added the route to delete a user.

--- routes/users.js
+++ routes/users.js
@@ -26,19 +26,19 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-//delete user
-// router.delete("/:id", async (req, res) => {
-//   if (req.body.userId === req.params.id || req.body.isAdmin) {
-//     try {
-//       await User.findByIdAndDelete(req.params.id);
-//       res.status(200).json("Account has been deleted");
-//     } catch (err) {
-//       return res.status(403).json(err);
-//     }
-//   } else {
-//     res.status(403).json("You can only delete your own account");
-//   }
-// });
+// delete user;
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(403).json(err);
+    }
+  } else {
+    res.status(403).json("You can only delete your own account");
+  }
+});
 
 // //get a user
 // router.get("/:id", async (req, res) => {
