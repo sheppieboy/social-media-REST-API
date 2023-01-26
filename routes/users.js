affe6f7251e505eb8b0abda2a72ec38c5f9ca510
Luke Sheppard <sheppiegit@gmail.com>
2023-01-25 19:04:59 -0500

    Added the route to get a user

--- routes/users.js
+++ routes/users.js
@@ -40,16 +40,16 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// //get a user
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const user = await User.findById(req.params.id);
-//     const { password, updatedAt, ...other } = user._doc;
-//     res.status(200).json(other);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //follow a user
 // router.put("/:id/follow", async (req, res) => {
